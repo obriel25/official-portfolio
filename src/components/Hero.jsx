b3165d8a9a64a2e35e160d95c681112d6fb329ca
Obriel <obrieljefferson@gmail.com>
2026-05-14 14:14:14 +0300

    My official portfolio website

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+    return(
+        <section className="min-h-screen flex flex-col justify-center items-center text-center bg-gray-950 text-white px-6 mt-16">
+            <h1 className="text-5xl font-bold mb-4">Hi,I'm Jefferson Obriel</h1>
+            <p className="text-lg text-gray-400 mb-8 max-w-xl">I'm a beginner web developer who love building things.</p>
+            <a href="#project" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg transition">View my work</a>
+        </section>
+    )
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+ return(
+    <nav  className="fixed top-0 w-full z-50 flex justify-between items-center px-8 py-4 bg-gray-900 text-white">
+        <h1 className="text-xl font-bold">JO</h1>
+        <ul className="flex gap-6">
+            <li><a href="#about" className="hover:text-blue-400">About</a></li>
+            <li><a href="#project"className="hover:text-blue-400">Project</a></li>
+            <li><a href="#contact"className="hover:text-blue-400">Contact</a></li>
+        </ul>
+    </nav>
+ );
+}
+
+export default Navbar;
