b3d83e0fc06a0e7af88698d510489d1b4ac7e858
Obriel <obrieljefferson@gmail.com>
2026-05-14 16:55:14 +0300

    Add a portfolio image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
+import photo from "../assets/photo.png"
+
 const Hero = () => {
     return(
         <section className="min-h-screen flex flex-col justify-center items-center text-center bg-gray-950 text-white px-6 mt-16">
+            <img src={photo} alt="profilephoto" className="w-40 h-40 rounded-full object-cover mb-6 border-4 border-blue-400" />
             <h1 className="text-5xl font-bold mb-4">Hi,I'm Jefferson Obriel</h1>
             <p className="text-lg text-gray-400 mb-8 max-w-xl">I'm a beginner web developer who love building things.</p>
-            <a href="#project" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg transition">View my work</a>
+            <div className="flex gap-6">
+                  <a href="#project" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg transition">View my work</a>
+                  <a href="#contact" className="border border-blue-400 text-blue-400 hover:bg-blue-400 hover:text-white font-semibold px-6 py-3 rounded-lg transition">Download CV</a>
+            </div>
+          
         </section>
     )
 }
